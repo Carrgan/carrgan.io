3160b8fcc5c378d317b3c4da75134bdddc234b54
carrgan <carrgan@outlook.com>
2023-07-02 18:59:31 +0800

    Improver performance

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,19 @@
 import React, { Suspense, lazy } from "react";
-import Layout from "@theme/Layout";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import BrowserOnly from "@docusaurus/BrowserOnly";
 import FullScreenLoader from "@site/src/components/common/FullScreenLoader";
 const Hello = lazy(() => import("@site/src/components/hello"));
+const Layout = lazy(() => import("@theme/Layout"));
 
 export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout description="Description will go into a meta tag in <head />">
-      <BrowserOnly>
-        {() => (
-          <Suspense fallback={<FullScreenLoader />}>
+    <BrowserOnly>
+      {() => (
+        <Suspense fallback={<FullScreenLoader />}>
+          <Layout description="Description will go into a meta tag in <head />">
             <Hello />
-          </Suspense>
-        )}
-      </BrowserOnly>
-    </Layout>
+          </Layout>
+        </Suspense>
+      )}
+    </BrowserOnly>
   );
 }
